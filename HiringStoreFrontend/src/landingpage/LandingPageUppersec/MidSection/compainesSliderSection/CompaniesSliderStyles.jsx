b8a39fe09efeb363edddaccaa629cef272ead399
Ankush-25 <repppol.me@gmail.com>
2025-03-02 15:26:00 +0530

    Some More Ui changes

--- HiringStoreFrontend/src/landingpage/LandingPageUppersec/MidSection/compainesSliderSection/CompaniesSliderStyles.jsx
+++ HiringStoreFrontend/src/landingpage/LandingPageUppersec/MidSection/compainesSliderSection/CompaniesSliderStyles.jsx
@@ -5,6 +5,8 @@ export const SingleCompanyComp = styled.div`
     height : 100px;
     width : 100px
     overflow: hidden;
+    padding: 12px;
+    border-radius: 5px;
        .CompaniesImages {
             height: 100px;
             filter: drop-shadow(5px 5px 10px rgba(255, 255, 255, 0.5));
@@ -15,6 +17,7 @@ export const SingleCompanyComp = styled.div`
 export const OuterSliderComCover = styled.div`
  display: flex;
  width: 1200px;
+ gap: 25px;
  margin: auto;
  height: 133px;    
  overflow: hidden;
