d29ded532aefbc2c5bd02260ad9b4ee87ea8b156
Ankush-25 <repppol.me@gmail.com>
2025-03-10 22:02:17 +0530

    Hey some More changes

--- HiringStoreFrontend/src/landingpage/LandingPageUppersec/MidSection/compainesSliderSection/CompaniesComp.jsx
+++ HiringStoreFrontend/src/landingpage/LandingPageUppersec/MidSection/compainesSliderSection/CompaniesComp.jsx
@@ -1,22 +1,21 @@
 import { CompaniesLogoPaths } from "../../../../assets/Global_Need_files/ImagesPaths";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
-import { OuterSliderComCover, SingleCompanyComp, SliderButton } from "./CompaniesSliderStyles";
+import { CompaniesContainer, OuterSliderComCover, SingleCompanyComp, SliderButton } from "./CompaniesSliderStyles";
 import "./CompaniesSlider.css"
 export function CompaniesHelpSlider(){
     const CompaniesEntries = Object.entries(CompaniesLogoPaths)
-    console.log(CompaniesEntries)
+
     return (
         <>
             <div className="CompaniesComTitle">
                 <p>Companies We've Helped</p>
             </div>
-            
             <OuterSliderComCover>
                 <SliderButton>
                     <FontAwesomeIcon icon={faChevronLeft} />
                 </SliderButton>
-                
+                <CompaniesContainer >
                     {CompaniesEntries.map(([companyName, logoPath], index) => (
                         <SingleCompanyComp key={index}>
                             <img 
@@ -26,6 +25,7 @@ export function CompaniesHelpSlider(){
                             />
                         </SingleCompanyComp>
                     ))}
+                </CompaniesContainer>   
                 <SliderButton>
                     <FontAwesomeIcon icon={faChevronRight} />
                 </SliderButton>
