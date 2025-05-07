8a5cd014aaf5da21602603553bb31f3a18b102b1
Ankush-25 <repppol.me@gmail.com>
2025-05-07 23:40:39 +0530

    footer added

---
+++ HiringStoreFrontend/src/landingpage/LandingPageLowersec/FooterSec.jsx
@@ -0,0 +1,9 @@
+import {FooterWrapper} from "./FooterSecStyles.jsx"
+export function Footerbody () {
+    return(
+        <FooterWrapper>
+            Hello brother How are You
+        </FooterWrapper>
+    
+    )
+}
---
+++ HiringStoreFrontend/src/landingpage/LandingPageLowersec/FooterSecStyles.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+export const FooterWrapper = styled.div`
+    border: red solid;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+`
